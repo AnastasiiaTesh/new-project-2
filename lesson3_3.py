d91acb7af634fb8785d4a8b2dc768a1baaf5292a
Anastasiia Teshaeva <ateshaeva@yandex.ru>
2022-06-07 10:20:51 +0700

    initial

---
+++ lesson3_3.py
@@ -0,0 +1,38 @@
+
+def operFunc():
+	oper = str(input('Выберите операцию: '))
+	count = 0	
+	if oper == '+':
+		N = int(input('Сколько операндов? '))
+		for num in range(1, N+1):
+			print('Введите операнд', num, end = '')
+			a = int(input(': '))
+			count += a		
+		print('Сумма:', count)
+	elif oper == '-':
+		N = int(input('Сколько операндов? '))
+		for num in range(1, N+1):
+			print('Введите операнд', num, end = '')
+			a = int(input(': '))
+			count -= a		
+		print('Сумма:', count)
+	elif oper == '*':
+		N = int(input('Сколько операндов? '))
+		for num in range(1, N+1):
+			print('Введите операнд', num, end = '')
+			a = int(input(': '))
+			count *= a		
+		print('Сумма:', count)
+	elif oper == '/':
+		N = int(input('Сколько операндов? '))
+		for num in range(1, N+1):
+			print('Введите операнд', num, end = '')
+			a = int(input(': '))
+			count /= a		
+		print('Сумма:', count)
+	else:
+		print('Ошибка: такой операции не существует. Попробуйте ещё раз.')
+		operFunc()
+
+
+operFunc()
